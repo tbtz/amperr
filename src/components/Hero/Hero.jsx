31228925c9515734c85e6dbf6bf56ee6ac944bd1
Bautz, Tom <tom.bautz@outlook.com>
2019-05-05 17:00:56 +0200

    fix 100vh bug

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import './Hero.scss';
 import { Flash } from '../Icons'
 
-function Hero() {
-    return (
-        <header className='hero'>
-            <h1 id='heading'>
-                amperr
+class Hero extends React.Component {
+    constructor(props) {
+        super(props);
+        this.hero = React.createRef();
+    }
+
+    componentDidMount() {
+        window.onresize = () => {
+            this.hero.current.style.height = window.innerHeight + 'px';
+        }
+        window.onresize();
+    }
+
+    render() {
+        return (
+            <header className='hero' ref={this.hero}>
+                <h1 id='heading'>
+                    amperr
                 <Flash color='#ffc400' />
-            </h1>
-        </header>
-    );
+                </h1>
+            </header>
+        );
+    }
 }
 
 export default Hero;
